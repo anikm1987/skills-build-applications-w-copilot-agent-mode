a5abedd82bc8889ce4adc6590fadc2a0d8e29b05
Aniket Mukherjee <mukherjee.aniket@gmail.com>
2026-01-20 18:04:29 +0000

    Add management command to populate MongoDB with OctoFit test data

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Workout, Leaderboard
+from django.db import connection
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Clear existing data
+
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User.objects.create(name='Spider-Man', email='spiderman@marvel.com', team=marvel),
+            User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel),
+            User.objects.create(name='Wonder Woman', email='wonderwoman@dc.com', team=dc),
+            User.objects.create(name='Batman', email='batman@dc.com', team=dc),
+        ]
+
+        # Create activities
+        Activity.objects.create(user=users[0], type='Running', duration=30, date='2026-01-20')
+        Activity.objects.create(user=users[1], type='Cycling', duration=45, date='2026-01-19')
+        Activity.objects.create(user=users[2], type='Swimming', duration=60, date='2026-01-18')
+        Activity.objects.create(user=users[3], type='Yoga', duration=20, date='2026-01-17')
+
+        # Create workouts
+        workout1 = Workout.objects.create(name='Cardio Blast', description='High intensity cardio workout')
+        workout2 = Workout.objects.create(name='Strength Training', description='Full body strength routine')
+        workout1.suggested_for.set([users[0], users[2]])
+        workout2.suggested_for.set([users[1], users[3]])
+
+        # Create leaderboard
+        Leaderboard.objects.create(team=marvel, points=150)
+        Leaderboard.objects.create(team=dc, points=120)
+
+
+
+        self.stdout.write(self.style.SUCCESS('Test data populated successfully.'))
